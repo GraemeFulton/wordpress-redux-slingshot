7f8e11b7560df187f0bb6add79893ee28465830c
Graeme Fulton <graemefulton89@gmail.com>
2017-02-25 23:57:19 +0000

    added dates

--- src/components/PostLoop/PostLoop.js
+++ src/components/PostLoop/PostLoop.js
@@ -9,14 +9,17 @@ class PostLoop extends React.Component {
     return (
       <div>
         <QueryPosts postType={'inspiration'} query={{search: '' }}/>
-        <h2>Posts</h2>
+        <h2>Inspiration</h2>
 
           {Object.keys(this.props.posts.items).map((item, id) => {
                  return (
                    <div key={id}>
-                      <p>{this.props.posts.items[item].title.rendered} </p>
-                      <img width={350} src={this.props.posts.items[item].og_image} />
-                    </div>
+                     <a href={this.props.posts.items[item].link} target={'_new'}>
+                        <p>{this.props.posts.items[item].title.rendered} </p>
+                        <img width={350} src={this.props.posts.items[item].og_image} />
+                      </a>
+                      <p>Source: {this.props.posts.items[item].og_site_name} | {this.props.posts.items[item].date}</p>
+                  </div>
                  );
              })
            }
